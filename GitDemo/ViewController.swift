dccc7c567ad4b94c30f4d89f007667a1f5e6eeee
vivek athilkar <vivek.athilkar200@gmail.com>
2019-09-28 17:30:04 +0530

    rahul merge into master commit and push

--- GitDemo/ViewController.swift
+++ GitDemo/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("demo git")
-        print("rahul branch")
+        print("rahul branch") //merge here
     }
 
 
